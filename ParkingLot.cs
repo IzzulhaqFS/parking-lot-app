2b826486027a15df26bb10bc74a1428d1021c9a9
Izzulhaq Fawwaz Syauqiy <izzulhaq219@gmail.com>
2025-03-06 20:55:21 +0700

    feature: add feature to get parking lot report

--- ParkingLot.cs
+++ ParkingLot.cs
@@ -22,7 +22,6 @@ namespace ParkingLotApp
                 Console.WriteLine("Sorry, parking lot is full");
                 return;
             }
-            
             var lotNumber = Enumerable.Range(1, _parkingLotSize).Except(_parkedVehicles.Keys).First();
             _parkedVehicles.Add(lotNumber, vehicle);
             Console.WriteLine($"Allocated slot number: {lotNumber}");
@@ -35,14 +34,73 @@ namespace ParkingLotApp
                 Console.WriteLine("Not found");
                 return;
             }
-            
             _parkedVehicles.Remove(lotNumber);
             Console.WriteLine($"Slot number {lotNumber} is free");
         }
 
         public void GetStatus()
         {
-            
+            Console.WriteLine("Slot No.\tRegistration No.\tType\tColor");
+            foreach (var vehicle in _parkedVehicles)
+            {
+                Console.WriteLine($"{vehicle.Key}\t\t{vehicle.Value.RegistrationNumber}\t\t{vehicle.Value.VehicleType}\t{vehicle.Value.Color}");
+            }
+        }
+
+        public void GetNumberOfVehiclesByType(string vehicleType)
+        {
+            var numberOfVehicles = _parkedVehicles.Values
+                .Count(vehicle => vehicle.VehicleType == Enum.Parse<VehicleType>(vehicleType));
+            Console.WriteLine(numberOfVehicles);
+        }
+
+        public void GetOddRegistrationNumbers()
+        {
+            var registrationNumbers = _parkedVehicles.Values
+                .Where(vehicle => int.TryParse(
+                    new string(vehicle.RegistrationNumber.Where(char.IsDigit).ToArray()), 
+                    out var lastDigit) && lastDigit % 2 == 1)
+                .Select(vehicle => vehicle.RegistrationNumber);
+            var enumerable = registrationNumbers.ToList();
+            Console.WriteLine(enumerable.Any() ? string.Join(", ", enumerable) : "No vehicle found");
+        }
+        
+        public void GetEvenRegistrationNumbers()
+        {
+            var registrationNumbers = _parkedVehicles.Values
+                .Where(vehicle => int.TryParse(
+                    new string(vehicle.RegistrationNumber.Where(char.IsDigit).ToArray()), 
+                    out var lastDigit) && lastDigit % 2 == 0)
+                .Select(vehicle => vehicle.RegistrationNumber);
+            var enumerable = registrationNumbers.ToList();
+            Console.WriteLine(enumerable.Any() ? string.Join(", ", enumerable) : "No vehicle found");
+        }
+
+        public void GetRegistrationNumbersByColor(string color)
+        {
+            var registrationNumbers = _parkedVehicles.Values
+                .Where(vehicle => vehicle.Color.Equals(color, StringComparison.OrdinalIgnoreCase))
+                .Select(vehicle => vehicle.RegistrationNumber);
+            var enumerable = registrationNumbers.ToList();
+            Console.WriteLine(enumerable.Any() ? string.Join(", ", enumerable) : "No vehicle found");
+        }
+
+        public void GetLotNumbersByColor(string color)
+        {
+            var lotNumbers = _parkedVehicles
+                .Where(pair => pair.Value.Color.Equals(color, StringComparison.OrdinalIgnoreCase))
+                .Select(pair => pair.Key);
+            var enumerable = lotNumbers.ToList();
+            Console.WriteLine(enumerable.Any() ? string.Join(", ", enumerable) : $"No {color} vehicle found");
+        }
+
+        public void GetLotNumberByRegistrationNumber(string registrationNumber)
+        {
+            var lotNumber = _parkedVehicles
+                .FirstOrDefault(pair =>
+                    pair.Value.RegistrationNumber.Equals(registrationNumber, StringComparison.OrdinalIgnoreCase))
+                .Key;
+            Console.WriteLine(lotNumber > 0 ? lotNumber : "Not found");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿#nullable enable
+using System;
 
 namespace ParkingLotApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ParkingLot? parkingLot = null;
+
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (input == "exit") break;
+                if (input != null)
+                {
+                    string[] command = input.Split(' ');
+
+                    switch (command[0])
+                    {
+                        case "create_parking_lot":
+                            parkingLot = new ParkingLot(int.Parse(command[1]));
+                            break;
+                        case "park":
+                            var vehicle = new Vehicle(
+                                command[1], 
+                                Enum.Parse<VehicleType>(command[3]), 
+                                command[2]);
+                            parkingLot?.AddVehicle(vehicle);
+                            break;
+                        case "leave":
+                            parkingLot?.RemoveVehicle(int.Parse(command[1]));
+                            break;
+                        case "status":
+                            parkingLot?.GetStatus();
+                            break;
+                        case "type_of_vehicles":
+                            parkingLot?.GetNumberOfVehiclesByType(command[1]);
+                            break;
+                        case "registration_numbers_for_vehicles_with_ood_plate":
+                            parkingLot?.GetOddRegistrationNumbers();
+                            break;
+                        case "registration_numbers_for_vehicles_with_event_plate":
+                            parkingLot?.GetEvenRegistrationNumbers();
+                            break;
+                        case "registration_numbers_for_vehicles_with_colour":
+                            parkingLot?.GetRegistrationNumbersByColor(command[1]);
+                            break;
+                        case "slot_number_for_registration_number":
+                            parkingLot?.GetLotNumberByRegistrationNumber(command[1]);
+                            break;
+                        case "slot_numbers_for_vehicles_with_colour":
+                            parkingLot?.GetLotNumbersByColor(command[1]);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid command");
+                            break;
+                    }
+                }
+            }
         }
     }
 }
